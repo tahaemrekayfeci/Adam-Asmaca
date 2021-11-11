0f2891029550e718147ca9eb784a28f943e60f57
Taha Emre Kayfeci <taha.kayfeci@mrevyap.k12.tr>
2021-11-11 09:28:02 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿
+namespace Adam_Asmaca
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btn_basla = new System.Windows.Forms.Button();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 12);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(417, 110);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // btn_basla
+            // 
+            this.btn_basla.Location = new System.Drawing.Point(426, 12);
+            this.btn_basla.Name = "btn_basla";
+            this.btn_basla.Size = new System.Drawing.Size(125, 61);
+            this.btn_basla.TabIndex = 1;
+            this.btn_basla.Text = "Oyuna Başla";
+            this.btn_basla.UseVisualStyleBackColor = true;
+            this.btn_basla.Click += new System.EventHandler(this.btn_basla_Click);
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(12, 208);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(408, 39);
+            this.flowLayoutPanel2.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.flowLayoutPanel2);
+            this.Controls.Add(this.btn_basla);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Adam Asmaca";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button btn_basla;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Adam_Asmaca
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+         int sayi;
+         string sehir;
+         string[] sehirler = new string[6];
+       char[] alfabe = "ABCÇDEFGĞHİIJKLMNOÖPRSŞTUÜVYZ".ToCharArray();
+    
+        Random rastgele = new Random();
+        
+        private void btn_basla_Click(object sender, EventArgs e)
+        {
+            
+            sayi = rastgele.Next(0, 6);
+           sehir = sehirler[sayi];
+            for (int i = 0; i < alfabe.Length; i++)
+            {
+                
+                Button btn = new Button();
+                btn.Name = i.ToString();
+                btn.Text = alfabe[i].ToString();
+                btn.Width = 20;
+                btn.Height =20;
+                flowLayoutPanel1.Controls.Add(btn);
+            }
+            for (int i = 0; i < sehir.Length; i++)
+            {
+                TextBox txt = new TextBox();
+                txt.Name = i.ToString();
+                txt.Text = "";
+                txt.Width = 20;
+                txt.Height = 20;
+                flowLayoutPanel2.Controls.Add(txt);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+         
+            sehirler[0] = "Samsun";
+            sehirler[1] = "Sivas";
+            sehirler[2] = "Giresun";
+            sehirler[3] = "Ağrı";
+            sehirler[4] = "Sakarya";
+            sehirler[5] = "Yozgat";
+        }
+    }
+}
